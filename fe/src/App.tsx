dfd63f4f09dba8f60eeb8f126a4519279c2baad6
Dongseok Kang <dsk08208@gmail.com>
2026-04-02 00:44:38 +0900

    feat : FE 인증 모듈 구현 (로그인, 로그아웃, 라우트 가드) (#52)
    - axios, react-router-dom 의존성 추가
    - authStore: Access Token 메모리 저장 관리
    - API 클라이언트: Axios 인터셉터 (AT 헤더 첨부, 401 시 reissue 후 재시도)
    - AuthProvider: 인증 상태 관리 및 앱 시작 시 reissue 시도
    - PrivateRoute / PublicRoute 라우트 가드
    - 로그인 페이지: 아이디/비밀번호 폼, 에러 메시지, Enter 키 제출
    - 메인 페이지: 헤더 (로고 + 로그아웃 버튼)
    
    #34 #35 #36

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,9 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./auth/AuthProvider";
+import { PrivateRoute } from "./auth/PrivateRoute";
+import { PublicRoute } from "./auth/PublicRoute";
+import { LoginPage } from "./pages/LoginPage";
+import { MainPage } from "./pages/MainPage";
+
 function App() {
   return (
-    <div>
-      <h1>Orino</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route element={<PublicRoute />}>
+            <Route path="/login" element={<LoginPage />} />
+          </Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<MainPage />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
