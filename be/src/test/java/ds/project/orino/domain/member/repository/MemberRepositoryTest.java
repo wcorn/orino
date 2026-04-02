bf1df823ddc424f4d0bbfeaefe5786bdc03e8ad6
wcorn <dsk08208@gmail.com>
2026-04-03 00:33:17 +0900

    fix : 인증 모듈 전체 변경사항 main 머지
    - PR 체인이 중간 브랜치끼리만 머지되어 main 미반영된 문제 수정
    - BE: Spring Security, JWT, 인증 API, Redis RT 관리
    - FE: 로그인/로그아웃, 인증 상태 관리, 라우트 가드
    - PasswordEncoderConfig 중복 빈 제거
    - MemberRepositoryTest에 TestRedisConfig 추가
    
    #69

--- be/src/test/java/ds/project/orino/domain/member/repository/MemberRepositoryTest.java
+++ be/src/test/java/ds/project/orino/domain/member/repository/MemberRepositoryTest.java
@@ -1,10 +1,12 @@
 package ds.project.orino.domain.member.repository;
 
+import ds.project.orino.config.TestRedisConfig;
 import ds.project.orino.domain.member.entity.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestRedisConfig.class)
 @Transactional
 class MemberRepositoryTest {
 
