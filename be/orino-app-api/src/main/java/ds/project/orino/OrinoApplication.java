52c2dca737933af340d41917bb9c7895413ddb2c
wcorn <dsk08208@gmail.com>
2026-04-04 14:56:22 +0900

    fix : ManagementContextConfiguration에 @Configuration 추가 및 스캔 제외
    - ActuatorSecurityConfig에 @ManagementContextConfiguration 복원
    - management child context에서 SecurityFilterChain 로드 보장
    - main context에서 ComponentScan 제외하여 UnreachableFilterChainException 방지
    
    #110

--- be/orino-app-api/src/main/java/ds/project/orino/OrinoApplication.java
+++ be/orino-app-api/src/main/java/ds/project/orino/OrinoApplication.java
@@ -1,11 +1,16 @@
 package ds.project.orino;
 
+import ds.project.orino.core.security.ActuatorSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@ComponentScan(excludeFilters = @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE, classes = ActuatorSecurityConfig.class))
 public class OrinoApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrinoApplication.class, args);
--- be/orino-core-web/src/main/java/ds/project/orino/core/security/ActuatorSecurityConfig.java
+++ be/orino-core-web/src/main/java/ds/project/orino/core/security/ActuatorSecurityConfig.java
@@ -1,9 +1,11 @@
 package ds.project.orino.core.security;
 
+import org.springframework.boot.actuate.autoconfigure.web.ManagementContextConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@ManagementContextConfiguration(proxyBeanMethods = false)
 public class ActuatorSecurityConfig {
 
     @Bean
