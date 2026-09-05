0589da3ae4ed1dcbb0e25e4fe45c93ef34330d68
wcorn <dsk08208@gmail.com>
2026-09-06 02:34:58 +0900

    test : 테스트가 끝날 때 남기는 소음을 지운다
    - create-drop이 셧다운 훅에서 날리는 drop이 이미 내려간 컨테이너를
      만나 HHH000478을 테이블 수만큼 찍던 것을 create로 바꿔 없앤다
    - 컨테이너는 어차피 폐기되므로 종료 시 drop은 얻는 것이 없다
    - MySQLContainer의 deprecated String 생성자를 DockerImageName으로 교체
    
    #1350

--- be/orino-domain-rdb/src/test/java/ds/project/orino/domain/support/TestMySqlConfig.java
+++ be/orino-domain-rdb/src/test/java/ds/project/orino/domain/support/TestMySqlConfig.java
@@ -4,13 +4,16 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestMySqlConfig {
 
+    private static final DockerImageName IMAGE = DockerImageName.parse("mysql:8.4.4");
+
     @Bean
     @ServiceConnection
     public MySQLContainer<?> mysqlContainer() {
-        return new MySQLContainer<>("mysql:8.4.4");
+        return new MySQLContainer<>(IMAGE);
     }
 }
